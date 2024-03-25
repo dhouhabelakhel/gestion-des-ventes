1551c13e93de503be9654a9c84448dac15bea477
dhouha belakhel <dhouhabelakhel2001@gmail.com>
2024-03-25 21:30:09 +0100

    add command total form

--- act.py
+++ act.py
@@ -42,11 +42,13 @@ def verif_qte(commande):
 
         
 def add_commande(code,commande):
+    print(code)
     for client in clients:
         if client['code']==code:
+            print('done')
             if verif_qte(commande):
-                client['commande'].append({'num_cmd':len(commandes)+1,'commande':commande})
-                print(client['commande'])
+                client['commandes'].append({'num_cmd':len(commandes)+1,'commande':commande})
+                commandes.append({'num_cmd':len(commandes)+1,'commande':commande})
                 print(client)
  
 
@@ -61,32 +63,33 @@ def total_cmd(num_cmd):
     for client in clients:
         for commande in client['commandes']:
             if commande['num_cmd']==num_cmd:
-                for article in commande['articles']:
+                for article in commande['commande']['articles']:
                     for prix_article in articles:
-                        if prix_article['code']==article['code']:
-                            total=total+(prix_article['PU']*article['qte_cmd'])
-    return total
+                        if prix_article['code_article']==article['code_article']:
+                            total=total+(prix_article['prix_unitaire']*article['qte_cmd'])
+    total_label=Label(app,text=total).grid(row=2,column=1)
 def affiche(code):
+    clear_interface() 
     for client in clients:
         if client['code']==code:
-            print('nom :'+client['nom'])
-            print('prenom :'+client['prenom'])
-            print('adresse :'+client['adresse'])
+            nom=Label(app,text=client['nom']).grid(row=2,column=1)
+            prenom=Label(app,text=client['prenom']).grid(row=3,column=1)
+            adresse=Label(app,text=client['adresse']).grid(row=4,column=1)
             for commande in client['commandes']:
-                print('num_commande :'+commande['num_cmd'])
-                print('date'+commande['date'])
-                print('total de la commande : '+total_cmd(commande['num_cmd']))
+                num_cmd=Label(app,text=commande['num_cmd']).grid(row=5,column=1)
+                date=Label(app,text=commande['commande']['date']).grid(row=6,column=1)
+                total=Label(app,text=total_cmd(commande['num_cmd'])).grid(row=7,column=1)
 def nbr_cmd(code):
     nbre=0
     for client in clients:
         if client['code']==code:
-            nbre=len(client['commnade'])
+            nbre=len(client['commandes'])
     return nbre
 def nbr_cmd_client(code,nom):
     nbre=0
     for client in clients:
         if client['code']==code and client['nom']==nom:
-            nbre=len(client['commnade'])
+            nbre=len(client['commnades'])
     return nbre
 
 app=Tk()
@@ -110,7 +113,7 @@ def add_client_form():
     nom=Entry(textvariable=nom_value).grid(row=2,column=2)
     prenom=Entry(textvariable=prenom_value).grid(row=3,column=2)
     code=Entry(textvariable=adresse_value).grid(row=4,column=2)
-    confirm_button=Button(text="add",padx=5,pady=5 ,command=lambda:add_client({'code': code_value.get(), 'nom': nom_value.get(), 'prenom': prenom_value.get(), 'adresse': adresse_value.get(), 'commande': []}))
+    confirm_button=Button(text="add",padx=5,pady=5 ,command=lambda:add_client({'code': code_value.get(), 'nom': nom_value.get(), 'prenom': prenom_value.get(), 'adresse': adresse_value.get(), 'commandes': []}))
     confirm_button.place(x=100, y=280)
 #https://www.plus2net.com/python/tkinter-DateEntry.php
 def get_selected_date():
@@ -136,6 +139,7 @@ def add_command_form(code=None):
     num_cmd_label=Label(app,text=f"Commande n° : {len(commandes)+1}",font="Script 20 bold", fg="#bd0b49",pady=10).grid(row=1,column=1)
     code_client_label = Label(app,text="Code " ,font="Script 25 bold", fg="#bd0b49",pady=10).grid(row=2,column=1)
     code_value=StringVar()
+    
     code_entry=Entry(textvariable=code_value)
     code_entry.grid(row=2,column=2)
     date_label = Label(app,text="Date " ,font="Script 25 bold", fg="#bd0b49",pady=10).grid(row=3,column=1)
@@ -147,20 +151,44 @@ def add_command_form(code=None):
     articles_select.grid(row=4,column=2)
     #https://stackoverflow.com/questions/40641130/how-to-use-a-comboboxselected-virtual-event-with-tkinter
     articles_select.bind("<<ComboboxSelected>>",lambda event: show_article(event, articles_select))
-    confirm_button=Button(text="add",command=lambda:add_commande(code,
+    confirm_button=Button(text="add",command=lambda:add_commande(code_value.get(),
                                                                  {'date':get_selected_date(),
-                                                                  'articles':selected_articles })).grid(row=5,column=1)
-   
+                                                                  'articles': selected_articles })).grid(row=5,column=1)
     if code is not None:
-        code_value.set(code)
-        code_entry.configure(state='readonly')
+       code_value.set(code)
+       code_entry.configure(state='readonly')
+   
         
+
+def home_interface():
+    clear_interface()
+    columns=('code','nom','prenom','adresse','nombre commande')
+    tree = ttk.Treeview(app, columns=columns, show="headings")
+    for col in columns:
+        tree.heading(col,text=col)
+    tree.pack(expand=True, fill=tk.BOTH)
+    for client in clients:
+        code = client['code']
+        nom = client['nom']
+        prenom = client['prenom']
+        adresse = client['adresse']
+        nb_commandes =nbr_cmd(client['code'])
+        tree.insert("", "end", values=(code, nom, prenom, adresse, nb_commandes))
+def command_total_form():
+    clear_interface()
+    num_cmd_label=Label(app,text="Numero commande").grid(row=1,column=1)
+    num_cmd_value=StringVar()
+    num_cmd_entry=Entry(textvariable=num_cmd_value).grid(row=1,column=2)
+    confirm_button=Button(text="montant ",padx=5,pady=5 ,command=lambda:total_cmd(num_cmd_value.get())).grid(row=1,column=3)
     
 menu = tk.Menu(app)
 file_menu = tk.Menu(menu, tearoff=0)
 menu.add_cascade(label="Menu", menu=file_menu)
+file_menu.add_command(label="Home", command=home_interface)
 file_menu.add_command(label="Add Client", command=add_client_form)
 file_menu.add_command(label="Add Command", command=add_command_form)
+file_menu.add_command(label="command total ", command=command_total_form)
 
+app.geometry("1000x500")
 app.config(menu=menu)
 app.mainloop()
